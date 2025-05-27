61a30398f59f7475245f473e516b3d4aa5d29601
Bot2048 <arpanjain0001@gmail.com>
2025-05-28 02:08:38 +0530

    fix: remove .prettierrc and dist from version control

--- dist/nostr-components.es.js
+++
@@ -1,2 +0,0 @@
-import"./components/nostr-profile-badge.es.js";import"./components/nostr-post.es.js";import"./components/nostr-profile.es.js";import"./components/nostr-follow-button.es.js";import"./assets/nostr-service-CHwVBmgX.js";import"./assets/utils-Cq_G6E5K.js";import"./assets/theme-DreKaX_O.js";
-//# sourceMappingURL=nostr-components.es.js.map
