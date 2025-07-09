b4ac0dfee1281d368d3414806bc0a43e1c3dcc97
Bot2048 <arpanjain0001@gmail.com>
2025-07-10 03:53:54 +0530

    removed zap to avoid interference from zap feature

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import NostrProfileBadge from './nostr-profile-badge/nostr-profile-badge.ts';
 import NostrPost from './nostr-post/nostr-post.ts';
 import NostrProfile from './nostr-profile/nostr-profile.ts';
 import NostrFollowButton from './nostr-follow-button/nostr-follow-button.ts';
-import NostrZap from './nostr-zap/nostr-zap.ts';
 import './onboarding/onboarding-modal.ts';
 
 export { NostrProfileBadge, NostrPost, NostrProfile, NostrFollowButton, NostrZap };
